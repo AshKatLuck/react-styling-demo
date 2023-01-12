b5af33cfcbe3ef89acc67fb87187b19de03cad28
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-01-11 19:44:35 -0800

    remove inline styles and add dynamic css classes

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -26,16 +26,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: isValid ? "black" : "red" }}>Course Goal</label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler}
-          style={{
-            borderColor: isValid ? "black" : "red",
-            background: isValid ? "transparent" : "salmon",
-          }}
-        />
+      <div className={`form-control ${isValid ? "" : "invalid"}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
